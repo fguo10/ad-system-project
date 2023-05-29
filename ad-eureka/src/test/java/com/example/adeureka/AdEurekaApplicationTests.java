f5c7710914c195c4b15c4ca5565df80281e9adef
FanGuo <fguo10@hawk.iit.edu>
2023-05-28 23:39:20 -0500

    create ad-eureka project: condifure with single server

---
+++ ad-eureka/src/test/java/com/example/adeureka/AdEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
