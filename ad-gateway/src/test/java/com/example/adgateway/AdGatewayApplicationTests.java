849820540ea77da698284e51eed1b2e1e617fc44
FanGuo <fguo10@hawk.iit.edu>
2023-05-30 13:53:44 -0500

    add ad-gateway

---
+++ ad-gateway/src/test/java/com/example/adgateway/AdGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
