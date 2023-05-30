18a1e062febaf805330f70963bed9510a2bd8fa3
FanGuo <fguo10@hawk.iit.edu>
2023-05-30 14:55:35 -0500

    add common response feature

---
+++ ad-services/ad-common/src/main/java/com/example/adcommon/annotation/IgnoreResponsAdvice.java
@@ -0,0 +1,21 @@
+package com.example.adcommon.annotation;
+
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+
+
+// @interface used to define a new annotation type.
+// @Target({ElementType.TYPE, ElementType.METHOD}): specific custom annotation can be applied to both classes and methods.
+// @Retention(RetentionPolicy.RUNTIME): annotation will be available at runtime
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface IgnoreResponsAdvice {
+
+
+}
