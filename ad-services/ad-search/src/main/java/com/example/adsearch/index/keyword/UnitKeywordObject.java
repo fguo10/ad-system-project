ee0e4a8bf8ec285ba1b015b0473b0f9bef94db9a
FanGuo <fguo10@hawk.iit.edu>
2023-06-13 18:07:26 -0500

    create adunit keyword object

---
+++ ad-services/ad-search/src/main/java/com/example/adsearch/index/keyword/UnitKeywordObject.java
@@ -0,0 +1,11 @@
+package com.example.adsearch.index.keyword;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UnitKeywordObject {
+    private Long unitId;
+    private String keyword;
+}
