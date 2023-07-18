b89bbfda1351e1a4870c434e24f6da25f1792c62
FanGuo <fguo10@hawk.iit.edu>
2023-07-17 23:49:39 -0500

    add todo list in index component

--- ad-services/ad-search/src/main/java/com/example/adsearch/index/IndexAware.java
+++ ad-services/ad-search/src/main/java/com/example/adsearch/index/IndexAware.java
@@ -1,7 +1,7 @@
 package com.example.adsearch.index;
 
-// use Generic type
-// implement the CRUD for index
+// todo: 如果广告数据太多，JVM内存无法存储所有的索引数据，需要将索引迁移到Redis.
+// use Generic type implement the CRUD for index
 // not all the table and not all attributes need to create index
 public interface IndexAware<K, V> {
     V get(K key);
